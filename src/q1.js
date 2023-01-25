e406e71debc58d0edce8db4076fb739b044bc67f
MagantyGeetha <116940656+MagantyGeetha@users.noreply.github.com>
2023-01-25 11:59:46 +0800

    Update q1.js

--- src/q1.js
+++ src/q1.js
@@ -9,10 +9,23 @@
     - invoke the function "swap"
 */
 
-function swap(x, y){
+//function swap(x, y){
     // Task 1: Add code here
-}
+    
+    def swap_values(x, y):
+    if isinstance(x, (int, float)) and isinstance(y, (int, float)):
+        x, y = y, x
+        print("x =", x, "y =", y)
+    else:
+        return -1
+//}
 
 // Task 2: Add code here
+x = 10
+y = 20
+swap_values(x, y);
+x = "Hello"
+y = "world"
+swap_values(x, y)
 
 module.exports = swap;
