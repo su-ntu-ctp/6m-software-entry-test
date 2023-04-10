15e85dfdfb8f128d85b63202d7eabbb81faef353
CP Wong <38209341+cpwong@users.noreply.github.com>
2023-04-10 09:58:30 +0800

    Update q5.js

--- src/q5.js
+++ src/q5.js
@@ -2,7 +2,8 @@
 /*
     Task 1:
     - Import the file `external.js`.
-    - Destructure the imported function.
+    - Destructure the imported function from `external.js` to display the string
+      "I am printing something" on console.log.
 */
 
 // Task 1: Add code here
