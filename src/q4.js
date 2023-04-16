a1419e2a27b3c8a7b4b72b54865f4d72888966b0
LWJ1982 <iambornin1982@gmail.com>
2023-04-16 21:54:12 +0800

    ver 1

--- src/q4.js
+++ src/q4.js
@@ -4,26 +4,41 @@
     - Add a constructor to the Child class can calls super().
     - Implement a new function addNewAbilities(newAbility) where the new ability will be added to the Parent's #abilities array.     
 */
-class Parent{
-    abilities = []
+class Parent {
+  abilities = [];
 
-    constructor(){
-        this.abilities.push("Parenting");
-        this.abilities.push("Role modeling");
-    }
+  constructor() {
+    this.abilities.push("Parenting");
+    this.abilities.push("Role modeling");
+  }
 
-    showAbilities(){
-        console.log("These are the abilities:")
-        for(const a of this.abilities){
-            console.log(a);
-        }
+  showAbilities() {
+    console.log("These are the abilities:");
+    for (const a of this.abilities) {
+      console.log(a);
     }
+  }
 }
-
 const p = new Parent();
 p.showAbilities(); // Observe that this function prints "Parenting" and "Role modeling".
 
 // Task 1: Add code here
+//Implement a Child class that extends the Parent. Copy the parent code
+class Child {
+  abilities = [];
+  //Add a constructor to the Child class can calls super().
+  constructor() {
+    this.abilities.push("Parenting");
+    this.abilities.push("Role modeling");
+  }
+
+  showAbilities() {
+    console.log("These are the abilities:");
+    for (const a of this.abilities) {
+      console.log(a);
+    }
+  }
+}
 
 const c = new Child();
 c.addNewAbility("Dancing");
