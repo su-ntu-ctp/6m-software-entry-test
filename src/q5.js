2f545e36cd1ef181d29c43fb820149ac4b23ae6f
terencegaffudSU <99375248+terencegaffudSU@users.noreply.github.com>
2023-04-27 19:38:11 +0800

    Update q5.js

--- src/q5.js
+++ src/q5.js
@@ -2,6 +2,7 @@
 /*
     Task 1:
     - Link the file `external.js` to this file.
+    - To test if the linking works, open q5-output.html in the browser and it should show "Hello World!"
 */
 
 // Task 1: Add code here
