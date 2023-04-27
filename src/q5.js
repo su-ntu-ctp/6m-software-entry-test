eb5392518d592bce524b281c4d935f2a731292f4
terencegaffudSU <99375248+terencegaffudSU@users.noreply.github.com>
2023-04-27 19:36:41 +0800

    Update q5.js

--- src/q5.js
+++ src/q5.js
@@ -2,11 +2,9 @@
 /*
     Task 1:
     - Link the file `external.js` to this file.
-    - Destructure the imported function from `external.js` to display the string
-      "I am printing something" on console.log.
 */
 
 // Task 1: Add code here
 
 // Do not change the code below
-print();
+document.querySelector("#test").innerHTML = print();
