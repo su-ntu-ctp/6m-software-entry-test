c48f1657a348d51c743d5032dc6160b36a8cf1cb
Anusuyadevi2023 <133954938+Anusuyadevi2023@users.noreply.github.com>
2023-05-18 22:02:09 +0800

    Update q1.js

--- src/q1.js
+++ src/q1.js
@@ -11,8 +11,30 @@
 
 function swap(x, y){
     // Task 1: Add code here
+  if(( (typeof x) == ('number'))& ( (typeof y) == ('number'))){
+   x = x + y;
+   y = x - y;
+   x = x - y;
+   document.write("After Swap<br>");
+document.write("x = " + x+"<br>" ); 
+document.write("y = " + y+"<br>" ); 
+        return(1);
+}else{
+    return(-1);
+}
 }
 
 // Task 2: Add code here
-
+var x = 'a';
+var y = 2;
+var z = 0;
+document.write("Before Swap<br>");
+document.write("x = " + x+"<br>" ); 
+document.write("y = " + y+"<br>" ); 
+z = swap(x , y);
+if(z>0){
+document.write("Completed Succesfully<br>" );
+}else{
+document.write("Variables are not Numeric<br>" );
+}
 module.exports = swap;
