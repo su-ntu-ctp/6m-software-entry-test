1fb146095e6a4965109fcd4d8ea8d9a6cedf5729
seanwhs <54506937+seanwhs@users.noreply.github.com>
2023-07-04 04:42:51 +0800

    Update q3.js

--- src/q3.js
+++ src/q3.js
@@ -6,10 +6,18 @@
     - Implement a for-in to loop through the keys in "dog" object and print it with console.log()
 */
 
+
+
+
 const dog = {
     petName:"Bobby"
 }
 
 // Task 1: Add code here
-
+// Task 1: Add a new property "breed" with the value "Poodle"
+dog.breed = "Poodle";
 // Task 2: Add code here
+console.log("Keys in the 'dog' object:");
+for (let key in dog) {
+    console.log(key);
+}
