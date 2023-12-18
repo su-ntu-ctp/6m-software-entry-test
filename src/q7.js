d2bc94851d72a111b435346997991ac695de1c04
terencegaffudSU <99375248+terencegaffudSU@users.noreply.github.com>
2023-12-18 09:17:41 +0800

    Create q7.js

---
+++ src/q7.js
@@ -0,0 +1,21 @@
+let person = {
+  firstName : "Jane",
+  lastName :  "Smith",
+  hobby : "swimming"
+}
+
+/*
+Explanation:
+Destructuring is a way of extracting multiple values from data stored in objects and arrays. It allows us to have an array or object that we are working with, but we only need some of the items contained in these.
+- https://www.w3schools.com/react/react_es6_destructuring.asp
+
+The form of this function is using the arrow function format. It allows for short syntax among other things.
+- https://www.w3schools.com/react/react_es6_arrow.asp
+
+Task 1: Modify this function to use object destructuring for the inputs to display only the first name and hobby properties of the object to the console:
+*/
+const introduce = () => {
+}
+
+greet(person);
+//The output should be "Hello, my name is Jane and I love swimming!"
