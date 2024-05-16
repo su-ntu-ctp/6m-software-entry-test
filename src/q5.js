41b1b38e284e6541409cb95b9e0f543ef14fba35
terencegaffudSU <99375248+terencegaffudSU@users.noreply.github.com>
2024-05-16 10:55:17 +0800

    Update q5.js

--- src/q5.js
+++ src/q5.js
@@ -3,6 +3,7 @@
     Task 1:
     - Link the file `external.js` to this file.
     - To test if the linking works, open q5-output.html using VScode's Live Server extension. It should print "Hello World!"
+    - You can refer to this link: https://www.w3schools.com/js/js_modules.asp to check how imports and modules are done.
 */
 
 // Task 1: Add code here
