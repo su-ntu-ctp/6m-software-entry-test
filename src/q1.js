1c5e623cb47b0c4b9e6d3c6c28be5f15229a0eec
CP Wong <38209341+cpwong@users.noreply.github.com>
2024-07-25 01:26:19 +0800

    Update q1.js

--- src/q1.js
+++ src/q1.js
@@ -2,7 +2,7 @@
     Task 1
     - Create a function that would swap the value of x and y using only x and y as variables.
     - x and y must be numeric.
-    - return -1 if x and y is not numeric.
+    - return -1 if either x or y is not numeric.
     - print the swapped values in the console if both x and y are numeric.
     
     Task 2
